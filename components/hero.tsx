11a277245c34fac2efaecf2125085ee14f52ec11
sibemou igor <sibpro61@gmail.com>
2025-09-29 13:36:26 +0100

    add profile picture

--- components/hero.tsx
+++ components/hero.tsx
@@ -66,33 +66,15 @@ const Hero = () => {
         {/* Right Visual Element */}
         <div className="flex justify-center lg:justify-end mt-10 lg:mt-0">
           <div className="relative w-full max-w-[400px] h-[450px] sm:max-w-[450px] sm:h-[500px] lg:w-[550px] lg:h-[600px]">
-            {/* Black rounded container */}
+            {/* Black rounded container with image */}
             <div className="absolute inset-0 bg-black rounded-2xl sm:rounded-3xl lg:rounded-[32px] overflow-hidden">
-              {/* Glowing curved lines */}
-              <div className="absolute inset-0">
-                {/* Top curved line */}
-                <div className="absolute top-8 sm:top-10 lg:top-12 right-0 w-full h-28 sm:h-32 lg:h-40">
-                  <div className="absolute top-0 right-0 w-3/4 h-full rounded-l-full bg-gradient-to-l from-purple-400 via-pink-400 to-orange-400 opacity-80 blur-sm"></div>
-                  <div className="absolute top-0 right-0 w-3/4 h-full rounded-l-full bg-gradient-to-l from-purple-300 via-pink-300 to-orange-300"></div>
-                </div>
+              {/* Profile Image */}
+              <img
+                  src="/pp.jpg"
+                  alt="WASHINGTON Temfing Brenda - UI/UX Designer"
+                  className="absolute inset-0 w-full h-full object-cover"
+              />
 
-                {/* Middle curved section */}
-                <div className="absolute top-1/3 right-0 w-full h-36 sm:h-40 lg:h-48">
-                  <div className="absolute top-0 right-0 w-4/5 h-full rounded-l-full bg-gradient-to-l from-purple-500 via-purple-400 to-pink-400 opacity-60 blur-md"></div>
-                  <div className="absolute top-0 right-0 w-4/5 h-full rounded-l-full bg-gradient-to-l from-purple-400 via-purple-300 to-pink-300 opacity-80"></div>
-                </div>
-
-                {/* Bottom curved line */}
-                <div className="absolute bottom-8 sm:bottom-10 lg:bottom-12 left-0 w-full h-24 sm:h-28 lg:h-32">
-                  <div className="absolute bottom-0 left-0 w-2/3 h-full rounded-r-full bg-gradient-to-r from-purple-400 via-pink-400 to-purple-300 opacity-70 blur-sm"></div>
-                  <div className="absolute bottom-0 left-0 w-2/3 h-full rounded-r-full bg-gradient-to-r from-purple-300 via-pink-300 to-purple-200"></div>
-                </div>
-
-                {/* Additional glow effects */}
-                <div className="absolute top-1/4 right-1/4 w-32 sm:w-36 lg:w-40 h-32 sm:h-36 lg:h-40 bg-purple-400 rounded-full opacity-20 blur-xl"></div>
-                <div className="absolute bottom-1/3 left-1/4 w-24 sm:w-28 lg:w-32 h-24 sm:h-28 lg:h-32 bg-pink-400 rounded-full opacity-25 blur-lg"></div>
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-40 sm:w-44 lg:w-48 h-40 sm:h-44 lg:h-48 bg-purple-500 rounded-full opacity-15 blur-2xl"></div>
-              </div>
             </div>
           </div>
         </div>
